438466cf9673e97da14a27b2492526e5c222e35a
rakib54 <rakibur5454@gmail.com>
2023-02-11 01:18:02 +0600

    leetcode problem move to oj folder

---
+++ online_judge_problem/as_far_as_possible_leetcode.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> grid{{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+    int n = grid.size();
+    queue<pair<int, int>> q;
+    vector<vector<int>> visited = grid;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (visited[i][j] == 1)
+            {
+                q.push({i, j});
+            }
+        }
+    }
+
+    if (q.size() == n * n)
+    { // if no water
+        return -1;
+    }
+    vector<pair<int, int>> dir{{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    int distance = -1;
+
+    while (!q.empty())
+    {
+        int size = q.size();
+
+        while (size--)
+        {
+            auto [x, y] = q.front();
+            q.pop();
+            for (auto [dx, dy] : dir)
+            {
+                int i = x + dx;
+                int j = y + dy;
+                if (i >= 0 && i < n && j >= 0 && j < n && visited[i][j] == 0)
+                {
+                    visited[i][j] = 1;
+                    q.push({i, j});
+                }
+            }
+        }
+        distance++;
+    }
+
+    cout << distance;
+
+    return 0;
+}
