b5cb62d617c3197063d668bf0082fe7b6884b0eb
rakib54 <rakibur5454@gmail.com>
2023-08-23 19:23:44 +0600

    priority queue problem Reorganize String is solved

--- online_judge_problem/Reorganize String.cpp
+++ online_judge_problem/Reorganize String.cpp
@@ -2,57 +2,69 @@
 using namespace std;
 #define ll long long
 
-class Solution {
+class Solution
+{
 public:
-    string reorganizeString(string s) {
-        int n = s.length();
-        vector<int>count(26,0);
+  string reorganizeString(string s)
+  {
+    int n = s.length();
+    vector<int> count(26, 0);
 
-        for(char ch: s){
-            count[ch-'a']++;
+    for (char ch : s)
+    {
+      count[ch - 'a']++;
 
-            if(count[ch-'a'] > (n+1)/2){
-                return "";
-            }
-        }
-
-
-        priority_queue<pair<int,char>>pq;
-
-        for(char c = 'a'; c<='z'; c++){
-            if(count[c-'a'] > 0){
-                pq.push({count[c-'a'], c});
-            }
-        }
-        string ans = "";
-
-        while(pq.size() >= 2){
-            auto p1 = pq.top();
-            pq.pop();
-            auto p2 = pq.top();
-            pq.pop();
-
-            ans += p1.second; p1.first--;
-            ans += p2.second; p2.first--;
-
-            if(p1.first > 0 ){
-                pq.push(p1);
-            }
-            if(p2.first > 0 ){
-                pq.push(p2);
-            }
-        }
-        if(!pq.empty()){
-            ans += pq.top().second;
-        }
-        
-        return ans;
+      if (count[ch - 'a'] > (n + 1) / 2)
+      {
+        return "";
+      }
     }
+
+    priority_queue<pair<int, char>> pq;
+
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+      if (count[c - 'a'] > 0)
+      {
+        pq.push({count[c - 'a'], c});
+      }
+    }
+    string ans = "";
+
+    while (pq.size() >= 2)
+    {
+      auto p1 = pq.top();
+      pq.pop();
+      auto p2 = pq.top();
+      pq.pop();
+
+      ans += p1.second;
+      p1.first--;
+      ans += p2.second;
+      p2.first--;
+
+      if (p1.first > 0)
+      {
+        pq.push(p1);
+      }
+      if (p2.first > 0)
+      {
+        pq.push(p2);
+      }
+    }
+    if (!pq.empty())
+    {
+      ans += pq.top().second;
+    }
+
+    return ans;
+  }
 };
 
-int main(){
+int main()
+{
   Solution solution;
-  solution.reorganizeString("aab")
- 
+  solution.reorganizeString("aab");
+
   return 0;
 }
