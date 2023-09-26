f66c0c84ab4778c7b6a857fbf99a08f5594b7f4e
rakib54 <rakibur5454@gmail.com>
2023-09-26 12:07:34 +0600

    leetcode problem Remove Duplicate Letters is solved

---
+++ String/Remove Duplicate Letters.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution
+{
+public:
+  string removeDuplicateLetters(string s)
+  {
+    string result;
+    vector<bool> taken(26, false);
+    vector<int> lastIndex(26);
+
+    for (int i = 0; i < s.size(); i++)
+    {
+      char ch = s[i];
+      lastIndex[ch - 'a'] = i;
+    }
+
+    for (int i = 0; i < s.size(); i++)
+    {
+      char ch = s[i];
+      int idx = ch - 'a';
+
+      if (taken[idx] == true)
+      {
+        continue;
+      }
+
+      while (result.size() > 0 && result.back() > ch && lastIndex[result.back() - 'a'] > i)
+      {
+        taken[result.back() - 'a'] = false;
+        result.pop_back();
+      }
+
+      result.push_back(ch);
+      taken[idx] = true;
+    }
+
+    return result;
+  }
+};
+
+int main()
+{
+  Solution solution;
+  solution.removeDuplicateLetters("bcabc");
+
+  return 0;
+}
---
+++ String/Smallest Subsequence of Distinct Characters.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution
+{
+public:
+  string smallestSubsequence(string s)
+  {
+    string result;
+    vector<bool> taken(26, false);
+    vector<int> lastIndex(26);
+
+    for (int i = 0; i < s.size(); i++)
+    {
+      char ch = s[i];
+      lastIndex[ch - 'a'] = i;
+    }
+
+    for (int i = 0; i < s.size(); i++)
+    {
+      char ch = s[i];
+      int idx = ch - 'a';
+
+      if (taken[idx] == true)
+      {
+        continue;
+      }
+
+      while (result.size() > 0 && result.back() > ch && lastIndex[result.back() - 'a'] > i)
+      {
+        taken[result.back() - 'a'] = false;
+        result.pop_back();
+      }
+
+      result.push_back(ch);
+      taken[idx] = true;
+    }
+
+    return result;
+  }
+};
+
+int main()
+{
+  Solution s;
+  s.smallestSubsequence("cbacdcbc");
+  return 0;
+}
