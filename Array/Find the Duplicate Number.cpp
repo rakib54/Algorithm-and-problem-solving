6e6cf9f31a0b2f7dd499c457169002b0d7f50333
rakib54 <rakibur5454@gmail.com>
2024-03-24 17:16:33 +0600

    Find the Duplicate Number is solved

--- Array/Find the Duplicate Number.cpp
+++ Array/Find the Duplicate Number.cpp
@@ -40,3 +40,24 @@ int main()
 
   return 0;
 }
+
+// another approach
+class Solution1
+{
+public:
+  int findDuplicate(vector<int> &nums)
+  {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 1; i++)
+    {
+      if (nums[i] == nums[i + 1])
+      {
+        return nums[i];
+      }
+    }
+
+    return -1;
+  }
+};
