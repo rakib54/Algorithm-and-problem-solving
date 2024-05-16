a75fe205725fcd0abc00e3725a7f7651efeb666e
rakib54 <rakibur5454@gmail.com>
2024-05-16 14:09:37 +0600

    binary tree problem Evaluate Boolean Binary Tree is solved

---
+++ Binary_Tree/Evaluate Boolean Binary Tree.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+  bool evaluateTree(TreeNode *root)
+  {
+    if (root->left == NULL && root->right == NULL)
+    {
+      return root->val;
+    }
+
+    int left = evaluateTree(root->left);
+    int right = evaluateTree(root->right);
+
+    if (root->val == 2)
+    {
+      return left | right;
+    }
+    return left & right;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
