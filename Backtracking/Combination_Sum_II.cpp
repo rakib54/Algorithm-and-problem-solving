9613814e6a20312be7b5116927dbeeaab6d679e8
rakib54 <rakibur5454@gmail.com>
2024-08-13 15:04:29 +0600

    added backtracking Combination Sum II problem's solution

--- Backtracking/Combination_Sum_II.cpp
+++ Backtracking/Combination_Sum_II.cpp
@@ -42,6 +42,42 @@ public:
   }
 };
 
+class Solution2 {
+public:
+    int n;
+    
+    void solve(int idx, vector<vector<int>>&result, vector<int>&curr, vector<int>& candidates, int target){
+        if(target < 0){
+            return;
+        }
+        if(target == 0){
+            result.push_back(curr);
+            return;
+        }
+
+        for(int i = idx; i < n ; i++){
+            if(i > idx && candidates[i] == candidates[i-1]){
+                continue;
+            }
+
+            curr.push_back(candidates[i]);  // do 
+            solve(i + 1, result, curr, candidates, target - candidates[i]); // explore
+            curr.pop_back(); // undo
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>>result;
+        vector<int>curr;
+        n = candidates.size();
+
+        solve(0,result,curr, candidates, target);
+
+        return result;
+    }
+};
+
 int main()
 {
   Solution solution;
