7e5f251be5528d523f392c4de8bc8b0c3a4afc09
rakib54 <rakibur5454@gmail.com>
2024-09-23 13:10:32 +0600

    leetcode dp problem Extra Characters in a String is solved

--- Dynamic_programming/Extra Characters in a String.cpp
+++ Dynamic_programming/Extra Characters in a String.cpp
@@ -54,6 +54,37 @@ public:
   }
 };
 
+class Solution2 {
+public:
+    int dp[51];
+    int solve(int i, string &s, unordered_set<string>&st, int &n){
+        if(i >= n) {
+            return 0;
+        } 
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        int result = 1 + solve(i + 1, s, st,n); // skip
+        
+        for(int j = i; j < n; j++){  // take
+            string curr = s.substr(i, j-i + 1);
+            if(st.count(curr)){
+                // valid substring
+                result = min(result, solve(j+1, s,st,n));
+            }
+        }
+
+        return dp[i] = result;
+    }
+    int minExtraChar(string s, vector<string>& dict) {
+        int n = s.size();
+        unordered_set<string>st(begin(dict), end(dict));
+        memset(dp, -1, sizeof(dp));
+        return solve(0,s, st, n);
+
+    }
+};
+
 int main()
 {
   Solution solution;
